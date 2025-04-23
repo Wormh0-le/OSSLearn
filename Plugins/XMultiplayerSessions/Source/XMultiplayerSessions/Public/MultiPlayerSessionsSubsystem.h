d357a92a26d80854be111003b924559305899428
wormh01e <yangxu@xverse.cn>
2025-04-23 18:04:35 +0800

    multiplayer session subsystem (GameInstanceSubsystem)

---
+++ Plugins/XMultiplayerSessions/Source/XMultiplayerSessions/Public/MultiPlayerSessionsSubsystem.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+
+#include "MultiPlayerSessionsSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XMULTIPLAYERSESSIONS_API UMultiPlayerSessionsSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	UMultiPlayerSessionsSubsystem();
+protected:
+
+private:
+	IOnlineSessionPtr SessionInterface;
+};
