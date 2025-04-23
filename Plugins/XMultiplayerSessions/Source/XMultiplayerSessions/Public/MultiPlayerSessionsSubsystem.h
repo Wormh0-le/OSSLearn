c2bf5a9f81fc4aae04108e00d5dbe6b46cdb7a49
wormh01e <yangxu@xverse.cn>
2025-04-23 20:46:31 +0800

    create menu

--- Plugins/XMultiplayerSessions/Source/XMultiplayerSessions/Public/MultiPlayerSessionsSubsystem.h
+++ Plugins/XMultiplayerSessions/Source/XMultiplayerSessions/Public/MultiPlayerSessionsSubsystem.h
@@ -17,8 +17,38 @@ class XMULTIPLAYERSESSIONS_API UMultiPlayerSessionsSubsystem : public UGameInsta
 	GENERATED_BODY()
 public:
 	UMultiPlayerSessionsSubsystem();
+
+	// To handle session functionality. The menu class will call these
+	void CreateSession(int32 NumPublicConnections, FString MatchType);
+	void FindSessions(int32 MaxSearchResults);
+	void JoinSession(const FOnlineSessionSearchResult& SessionResult);
+	void DestroySession();
+	void StartSession();
+
 protected:
+	// Internal callbacks for the delegates we'll add to the Online Session Interface delegate list.
+	// These don't need to be called outside this class.
+
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionsComplete(bool bWasSuccessful);
+	void OnjoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
 
 private:
 	IOnlineSessionPtr SessionInterface;
+
+	// To add to the Online Session Interface delegate list.
+	// We' ll bind Our MultiplayerSessionSubsystem internal callbacks to these.
+
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+	FOnFindSessionsCompleteDelegate FindSessionCompleteDelegate;
+	FDelegateHandle FindSessionsCompleteDelegateHandle;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+	FDelegateHandle StartSessionCompleteDelegateHandle;
 };
